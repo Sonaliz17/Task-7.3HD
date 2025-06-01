708cc3167da72f6573e3aa0d66955471f7f2c9ff
Sonaliz17 <sona.lizbethjoseph@gmail.com>
2025-06-01 17:30:31 +1000

    Update boards.test.js

--- Backend/_tests_/boards.test.js
+++ Backend/_tests_/boards.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../app');
 
 describe('Board Access', () => {
   it('should block access without token', async () => {

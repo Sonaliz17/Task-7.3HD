509efd0bf19e3ab5cce7affd716e9916e2e988ea
Sonaliz17 <sona.lizbethjoseph@gmail.com>
2025-06-01 16:13:54 +1000

    Create Login.test.js

---
+++ Frontend/src/_tests_/Login.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders login form', () => {
+  render(<App />);
+  expect(screen.getByText(/login/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+});

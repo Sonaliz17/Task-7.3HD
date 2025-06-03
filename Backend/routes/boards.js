fdf4e758a75ef2939a967617e3980bf56fd601a1
Sonaliz17 <sona.lizbethjoseph@gmail.com>
2025-06-03 19:41:24 +1000

    Update boards.js

--- Backend/routes/boards.js
+++ Backend/routes/boards.js
@@ -3,14 +3,7 @@ const Board = require('../models/Board');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
-// Helper to find board and handle not found
-const getBoard = async (req, res) => {
-  const board = await Board.findOne({ _id: req.params.id, user: req.user.id });
-  if (!board) return res.status(404).send('Board not found');
-  return board;
-};
-
-// ----- Board CRUD -----
+// Boards CRUD
 router.get('/', auth, async (req, res) => {
   const boards = await Board.find({ user: req.user.id });
   res.json(boards);
@@ -38,26 +31,25 @@ router.delete('/:id', auth, async (req, res) => {
   res.send('Board deleted');
 });
 
-// ----- Task CRUD -----
+// Tasks CRUD
 router.get('/:id/tasks', auth, async (req, res) => {
-  const board = await getBoard(req, res);
-  if (board) res.json(board.tasks);
+  const board = await Board.findOne({ _id: req.params.id, user: req.user.id });
+  if (!board) return res.status(404).send('Board not found');
+  res.json(board.tasks);
 });
 
 router.post('/:id/tasks', auth, async (req, res) => {
-  const board = await getBoard(req, res);
-  if (board) {
-    board.tasks.push({ text: req.body.text });
-    await board.save();
-    res.status(201).json(board.tasks);
-  }
+  const board = await Board.findOne({ _id: req.params.id, user: req.user.id });
+  if (!board) return res.status(404).send('Board not found');
+  board.tasks.push({ text: req.body.text });
+  await board.save();
+  res.json(board.tasks);
 });
 
 router.put('/:boardId/tasks/:taskId', auth, async (req, res) => {
   const board = await Board.findOne({ _id: req.params.boardId, user: req.user.id });
   const task = board?.tasks.id(req.params.taskId);
   if (!task) return res.status(404).send('Task not found');
-
   task.text = req.body.text ?? task.text;
   task.completed = req.body.completed ?? task.completed;
   await board.save();
@@ -68,11 +60,13 @@ router.delete('/:boardId/tasks/:taskId', auth, async (req, res) => {
   const board = await Board.findOne({ _id: req.params.boardId, user: req.user.id });
   const task = board?.tasks.id(req.params.taskId);
   if (!task) return res.status(404).send('Task not found');
-
   task.remove();
   await board.save();
   res.send('Task deleted');
 });
+
+module.exports = router;
+
 
 module.exports = router;
 
